15feddfcaa035a71b0af7e4a989f6fc0108ed604
Arturo Calderon <calderov@gmail.com>
2020-02-28 02:01:36 -0800

    First working implementation.
    This is the first working implementation. I added a few example images
    and a main.py file to start the program.

--- tessellate.py
+++ tessellate.py
@@ -1,18 +1,14 @@
+import random
 from PIL import Image
+from PIL import ImageDraw
 from PIL import ImageFilter
 
-# Define a few constants
-LUMINANCE_THRESHOLD = 128
-HORIZONTAL_SPLITS = 3
-VERTICAL_SPLITS = 4
-MIN_POINTS_PERCENT_PER_SECTION = 1
-
-def extract_points_from_edges(edges, luminance_threshold):
+def extract_points_from_edges(edges, luminanceThreshold):
 	points = []
 	maxX, maxY = edges.size
 	for x in range(maxX):
 		for y in range(maxY):
-			if edges.getpixel((x, y)) > luminance_threshold:
+			if edges.getpixel((x, y)) > luminanceThreshold:
 				points.append((x, y))
 	return points
 
@@ -22,28 +18,116 @@ def paint_points_to_image(points, size):
 		image.putpixel(point, 255)
 	return image
 
-def main():
-	# Load original image
-	image = Image.open('input.jpg')
+def getPointsInRegion(points, topLeft, bottomRight):
+	pointsInRegion = []
 
-	# Find the edges from the grayscale version of the image
+	minX, minY = topLeft
+	maxX, maxY = bottomRight
+
+	for x, y in points:
+		if minX <= x and x < maxX and minY <= y and y < maxY:
+			pointsInRegion.append((x, y))
+
+	return pointsInRegion
+
+def getTessellationSections(points, topLeft, bottomRight, pointsCountThreshold):
+	sections = []
+	pointsInRegion = getPointsInRegion(points, topLeft, bottomRight)
+
+	# print(len(pointsInRegion), " - ", pointsCountThreshold, " : ", (topLeft, bottomRight))
+
+	# breakpoint()
+	if len(pointsInRegion) > pointsCountThreshold:
+		x0, y0 = topLeft
+		x1, y1 = bottomRight
+
+		# Randomly choose in what direction to partiton
+		if random.random() < 0.5:
+			# Compute possible vertical partitions and recursivelly extract its tessellations
+			topMiddle = (x0 + int((x1 - x0) / 2), y0)
+			bottomMiddle = (x0 + int((x1 - x0) / 2), y1)
+			
+			leftSections = getTessellationSections(pointsInRegion, topLeft, bottomMiddle, pointsCountThreshold)
+			rightSections = getTessellationSections(pointsInRegion, topMiddle, bottomRight, pointsCountThreshold)
+			
+			sections += leftSections
+			sections += rightSections
+
+		else:
+			# Compute possible horizontal partitions and recursivelly extract its tessellations
+			middleLeft = (x0, y0 + int((y1 - y0) / 2))
+			middleRight = (x1, y0 + int((y1 - y0) / 2))
+
+			topSections = getTessellationSections(pointsInRegion, topLeft, middleRight, pointsCountThreshold)
+			bottomSections = getTessellationSections(pointsInRegion, middleLeft, bottomRight, pointsCountThreshold)
+
+			sections += topSections
+			sections += bottomSections
+
+	else:
+		sections.append((topLeft, bottomRight))
+
+	return sections
+
+def getColorFromSection(originalImage, topLeft, bottomRight):
+	r = 0
+	g = 0
+	b = 0
+	minX, minY = topLeft
+	maxX, maxY = bottomRight
+
+	totalPixels = max((maxX - minX) * (maxY - minY), 1)
+
+	for x in range(minX, maxX):
+		for y in range(minY, maxY):
+			pixel_r, pixel_g, pixel_b = originalImage.getpixel((x, y))
+			r += pixel_r
+			g += pixel_g
+			b += pixel_b
+
+	r = int(r / totalPixels)
+	g = int(g / totalPixels)
+	b = int(b / totalPixels)
+
+	return (r, g, b)
+
+
+def paintSections(originalImage, sections):
+	image = Image.new('RGB', originalImage.size)
+	draw = ImageDraw.Draw(image)
+
+	for section in sections:
+		topLeft, bottomRight = section
+		color = getColorFromSection(originalImage, topLeft, bottomRight)
+		draw.rectangle([topLeft, bottomRight], color)
+
+	return image
+	
+def tessellate(inputPath, outputPath, luminanceThreshold = 128, pointsCountThreshold = 10):
+	print("Load original image\n")
+	image = Image.open(inputPath)
+
+	print("Find the edges from the grayscale version of the image\n")
 	edges = image.convert('L').filter(ImageFilter.FIND_EDGES)
-
-	# Preview the edges
 	edges.show()
 
-	# Create a data structure of points with those pixels
-	# from the edges that are above the luminance threshold
-	points = extract_points_from_edges(edges, LUMINANCE_THRESHOLD)
+	print("Create a data structure of points with those pixels")
+	print("from the edges that are above the luminance threshold\n")
+	points = extract_points_from_edges(edges, luminanceThreshold)
 
-	# Paint the points into a new image (hard edges)
-	hardEdges = paint_points_to_image(points, image.size)
+	pointedImage = paint_points_to_image(points, image.size)
+	pointedImage.show()
 
-	# Preview hard edges
-	hardEdges.show('output.png')
+	print("Get a list of the sections that tessellate the image [(topLeft, bottomRight), ...]\n")
+	sections = getTessellationSections(points, (0,0), image.size, pointsCountThreshold)
 
-	print(len(points), "points found")
+	print("Image split into %d sections\n" % len(sections))
+
+	print("Paint sections to image\n")
+	tessellatedImage = paintSections(image, sections)
+
+	print("Save tessellated image\n")
+	tessellatedImage.save(outputPath)
+	tessellatedImage.show()
+
 	print('Done')
-
-if __name__ == '__main__':
-	main()
